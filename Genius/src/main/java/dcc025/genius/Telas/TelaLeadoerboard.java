7cdaf65598cfec09e028645f13c490cd1ec0f2e2
EnzoAraP <enzo.arap@gmail.com>
2024-09-30 16:51:35 -0300

    Retira  Leaderoard

--- Genius/src/main/java/dcc025/genius/Telas/TelaLeadoerboard.java
+++ Genius/src/main/java/dcc025/genius/Telas/TelaLeadoerboard.java
@@ -19,6 +19,8 @@ public class TelaLeadoerboard {
     private final TelaInicial telainicial;
     private final Font fonteGrande;
     private final Font fonteMedia;
+    protected JTable tabela;
+    
 
     public TelaLeadoerboard(TelaInicial telainicial) {
         this.telainicial = telainicial;
@@ -41,7 +43,29 @@ public class TelaLeadoerboard {
       panel.setLayout(new GridBagLayout());
       panel.setSize(WIDTH,HEIGHT);
       panel.setBackground(new Color(140, 200, 220, 80));
+      JLabel leaderboard = new JLabel("Leaderboard");
+      leaderboard.setFont(fonteGrande);
+        GridBagConstraints constraints = new GridBagConstraints();
+      {//add Titulo
+               JPanel panelregra = new JPanel();
+        panelregra.setLayout(new BorderLayout());
+              panelregra.setBackground(new Color(160, 165, 220));
+            
+        constraints.gridx = 1; // Grid column
+        constraints.gridy = 0; // Grid row
+        constraints.gridwidth = 2; 
+        constraints.gridheight = 1; 
+        constraints.insets = new Insets(10, 10, 10, 10); // Padding
+     
+        panelregra.add(leaderboard,BorderLayout.CENTER);
+        panel.add(panelregra, constraints);     
+        }
+      
       tela.add(panel);
+    }
+    public void adicionaTable()
+    {
+        
     }
     public void mostrarTudo()
     {
